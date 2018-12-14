05656bf7f47e994b88578ad1e0f1ff43ca054850
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2018-12-14 22:40:29 +0800

    Update router.py

--- config_file-router/router.py
+++ config_file-router/router.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'], endpoint='yuyu')  # @app.route()是thedecorator是装饰器功能
+@app.route('/index', methods=['GET', 'POST'], endpoint='yuyu')  # @app.route()是thedecorator是装饰器功能
 # 关键就是执行self.add_url_rule(rule, endpoint, f, **options)
 # app.add_url_rule('/', 'yuyu', index, methods=['GET', 'POST'])
 # return index
@@ -17,6 +17,11 @@ def index():
 #     return 'Hello World!
 # app.app.add_url_rule('/', 'yuyu', index, methods=['GET', 'POST'])
 # Flask路由添加是通过装饰器，但本质就是添加路由 self.add_url_rule
+
+# 重定向 redirect_to，如下查询index2时会自动跳转到index
+@app.route('/index2', methods=['GET', 'POST'], endpoint='yuyuyuyu', redirect_to='index')
+def index2():
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
