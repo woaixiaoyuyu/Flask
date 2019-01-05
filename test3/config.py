5211b6478ed88d53775b301ffd037148b59b31f5
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2019-01-05 19:57:07 +0800

    Add files via upload

---
+++ test3/config.py
@@ -0,0 +1,40 @@
+class Config:
+    SECRET_KEY = 'hard to guess string'
+
+    MAIL_SERVER = 'smtp.qq.com'
+    MALT_PORT = 465  # 465 or 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '519902366@qq.com'
+    MAIL_PASSWORD = 'khpvupznqlqmbiad'
+    FLASKY_ADMIN = '519902366@qq.com'  # this is the email of admin
+    FLASKY_MAIL_SENDER = '519902366@qq.com'  # this is sender
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'  # 只是在主题前面加个修饰前缀
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 静态方法 类或实例均可调用
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:xiaoyuyu@localhost/flask'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:xiaoyuyu@localhost/flask_test'
+
+
+class ProductionCongfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:xiaoyuyu@localhost/flask'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionCongfig,
+    'default': DevelopmentConfig
+}
